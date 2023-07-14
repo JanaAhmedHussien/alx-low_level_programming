2118f22e00e81ea0837c7a23f762ee598c709376
jana <jana.ahmedhussein244@gmail.com>
2023-07-14 16:00:22 -0700

    pp

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point.
+ *
+ * Description: skipping letter durind printing alphabets
+ *
+ * Return: 0 always(success)
+ */
+int main(void)
+{
+char al = 'a';
+while (al <= 'z')
+{
+if (al == 'q' || al == 'e')
+	al++;
+putchar(al);
+al++
+}
+putchar('\n');
+return (0);
+}
