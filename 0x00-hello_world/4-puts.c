4218d87e4eb07dd1e5f6bd025c74f9691fca6880
jana <jana.ahmedhussein244@gmail.com>
2023-07-13 17:27:27 -0700

    pp

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
+
 /**
- *main - Entry point
+ * main - Entry point
  *
- *Description: using puts function to print a sentence 
+ * Description: using puts function to print a sentence
  *
- *Return: always 0 (success)
-*/
+ * Return: always 0 (success)
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
+
