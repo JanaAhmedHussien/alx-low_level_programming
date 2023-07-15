4cc0169f891789bd72ea5f485b99b5cdfc0caa94
jana <jana.ahmedhussein244@gmail.com>
2023-07-14 17:59:21 -0700

    pp

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point.
+ *
+ * Description: printing single digits with connections.
+ *
+ * Return: 0 Always(success).
+ */
+int main(void)
+{
+int n = 0;
+while (n <= 9)
+{
+putchar(n + '0');
+putchar(", ");
+n++;
+
+}
+putchar('\n');
+return (0);
+}
