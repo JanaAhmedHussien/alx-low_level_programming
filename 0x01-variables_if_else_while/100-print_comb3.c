1627c022c58a709db345cff7b829444cd72724a3
jana <jana.ahmedhussein244@gmail.com>
2023-07-14 18:30:50 -0700

    pp

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,16 +10,16 @@ int main(void)
 {
 int n = 0;
 int n2 = 1;
-while (n <= 9)
+while (n <= '9')
 {
 
-while (n2 <= 8)
+while (n2 <= '8')
 {
-putchar(n);
-putchar(n2);
+putchar(n + '0');
+putchar(n2 + '1');
 n2++;
 }
-if (n != 9)
+if (n != '9')
 {
 putchar(',');
 putchar(' ');
