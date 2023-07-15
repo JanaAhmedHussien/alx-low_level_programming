bbfd9a2753cfa8330ad9134e5f8fb3288c8cd147
jana <jana.ahmedhussein244@gmail.com>
2023-07-14 17:07:57 -0700

    pp

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point.
+ *
+ * Description: mirror alphabets.
+ *
+ * Return: 0 Always(success).
+ */
+int main(void)
+{
+char al = 'z';
+while (al <= 'a')
+{
+putchar(al);
+al++;
+}
+putchar('\n');
+return (0);
+}
