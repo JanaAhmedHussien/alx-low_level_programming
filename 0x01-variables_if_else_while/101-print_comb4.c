3f5db8a9aca129fed54dfde7b33a98cf96d9945e
jana <jana.ahmedhussein244@gmail.com>
2023-07-14 19:10:51 -0700

    pp

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,9 +11,9 @@ int main(void)
 int i, j, k;
 for (i = 0; i <= 7; i++)
 {
-for (j += i; j <= 8; j++)
+for (j = i+1; j <= 8; j++)
 {
-for (k += j; k <= 9; k++)
+for (k = j+1; k <= 9; k++)
 {
 putchar(i + '0');
 putchar(j + '0');
