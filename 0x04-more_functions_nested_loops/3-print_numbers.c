7fe37c89da5fd4a12e610630cd3a0c0029d6056b
jana <jana.ahmedhussein244@gmail.com>
2023-07-20 14:03:51 -0700

    pp

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - function that prints the numbers, from 0 to 9
+ *
+ * Return: Always 0(success)
+ */
+
+void print_numbers(void)
+{
+for (int i = 0;i <= 9; i++)
+{
+printf("%d", i);
+}
+_putchar('\n');
+}
