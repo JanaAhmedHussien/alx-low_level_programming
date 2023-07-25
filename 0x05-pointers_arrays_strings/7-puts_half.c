2f0f7af900a5aeb105fdb313fa81bf003ac06e10
jana <jana.ahmedhussein244@gmail.com>
2023-07-24 18:02:11 -0700

    pp

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,9 +11,18 @@ void puts_half(char *str)
 int i = 0;
 while (str[i])
 i++;
+if (i % 2 == 0)
+{
 for (i /= 2; str[i] != '\0'; i++)
 {
 _putchar(str[i]);
+}
+}
+else
+{
+for ((i - 1) / 2; ((i - 1) / 2) < i -1;((i - 1) / 2)++)
+_putchar(str[((i - 1) / 2) + 1]);
+
 }
 _putchar('\n');
 
