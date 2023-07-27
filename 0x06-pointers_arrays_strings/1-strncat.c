3099c1870bec3b8bcd46198f1a38ff1db03e5031
jana <jana.ahmedhussein244@gmail.com>
2023-07-27 16:20:37 -0700

    pp

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strncat - function that concatenates two strings.
+ *
+ * @dest: first input
+ * @src: second input
+ * @n: third input
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+int c, c2;
+c = 0;
+while (dest[c])
+c++;
+for (c2 = 0; c2 < n && src[c2] != '\0'; c2++)
+dest[c + c2] = src[c2];
+dest[c + c2] = '\0';
+return (dest);
+}
