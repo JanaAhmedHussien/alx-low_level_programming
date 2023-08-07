081f94c64cdaf9625c09cfee76e8864f0d67ea8a
jana <jana.ahmedhussein244@gmail.com>
2023-08-07 14:34:35 -0700

    pp

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -7,15 +7,21 @@
  *
  * Return: an arrays of chars
  */
-
 char *create_array(unsigned int size, char c)
 {
-char *n = malloc(size);
-if (size == 0 || n == 0)
-return (0);
-while (size--)
-n[size] = c;
-return (n);
+char *array = NULL;
+unsigned int i;
 
-
+if (size == 0)
+return (NULL);
+if (size != 0)
+{
+array = (char *)malloc(size * sizeof(char));
+if (array != NULL)
+{
+for (i = 0; i < size; i++)
+array[i] = c;
+}
+}
+return (array);
 }
