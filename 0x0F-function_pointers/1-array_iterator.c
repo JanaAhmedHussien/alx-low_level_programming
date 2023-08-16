6783950693dfff947bd3641d7cd0cfdab4425b0b
jana <jana.ahmedhussein244@gmail.com>
2023-08-16 06:07:43 -0700

    Pp

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,10 +9,10 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-size_t i;
+int *end = array + size - 1;
 if (array, size, action)
 {
-for (i = 0; i < size; i++)
-action(array[i]);
+while (array <= end)
+action(*array++);
 }
 }
