a9673d1003fed3131ecab55963c001fb54090a8b
jana <jana.ahmedhussein255@gmail.com>
2023-08-24 16:48:13 -0700

    some changes

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,9 +11,7 @@ int string_length(char *s)
 {
 int i = 0;
 if (!s)
-{
 return (0);
-}
 while (*s++)
 i++;
 return (i);
@@ -30,7 +28,7 @@ size_t print_list(const list_t *h)
 size_t i = 0;
 while (h)
 {
-printf("[%d] %s \n", string_length(h->str), h->str ? h->str : "(nil)");
+printf("[%d] %s\n", string_length(h->str), h->str ? h->str : "(nil)");
 h = h->next;
 i++;
 }
