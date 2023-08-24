d3c149fcec09ceeea67704623b733f27a601852d
jana <jana.ahmedhussein255@gmail.com>
2023-08-24 15:16:13 -0700

    first task

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+/**
+ * string_length - function to measure the length of the string.
+ *
+ * @s: pointer to string we want to measure.
+ *
+ * Return: length of the string
+ *
+ */
+int string_length(char *s)
+{
+int i = 0;
+if (!s)
+{
+return (0);
+}
+while (*s++)
+i++;
+return (i);
+}
+/**
+ * print_list - prints linked lists
+ *
+ * @h: pointer for the first node.
+ *
+ * Return: linked lists.
+ */
+size_t print_list(const list_t *h)
+{
+size_t j = 0;
+while (h)
+{
+printf("[%d] %s \n", string_length(h->string), h->str ? h->str : "(nil)");
+h = h->next_node;
+j++;
+}
+return (j);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+hellooz data structure
