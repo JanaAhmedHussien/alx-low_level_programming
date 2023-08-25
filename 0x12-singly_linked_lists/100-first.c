61a0444b53ca6d158ecc9610187a53eba0b553f2
jana <jana.ahmedhussein255@gmail.com>
2023-08-24 17:36:19 -0700

    pp

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+/**
+ * test - function to make the task easier
+ *
+ * Description: print sentence
+ *
+ * Return: sentence
+ */
+void test(void)
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+}
