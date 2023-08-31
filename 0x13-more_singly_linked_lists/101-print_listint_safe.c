6e4feda6f179fd2443b8e21a83cdb8153a007a23
jana <jana.ahmedhussein255@gmail.com>
2023-08-30 18:51:52 -0700

    pp

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+/**
+ * print_listint_safe - prints a listint_t linked list.
+ * @head: pointer of the head of linked list
+ * Return: number of nodes in list
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+}
---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,8 @@
+#include "lists.h"
+/**
+ * free_listint_safe - frees a listint_t list.
+ * @h: pointer the head
+ * Return: the size of the list that was free’d
+ */
+size_t free_listint_safe(listint_t **h)
+{}
