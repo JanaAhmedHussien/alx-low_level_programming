d74d8372b12f44547cef22066b06cd74e5efee84
jana <jana.ahmedhussein244@gmail.com>
2023-10-16 12:09:10 -0700

    pp

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,31 @@
+#include "main.h"
+int add (int a, int b)
+{
+return a + b;
+}
+int sub(int a, int b)
+{
+return a - b;
+}
+int mul(int a, int b)
+{
+return a * b;
+}
+int division(int a, int b)
+{
+if (b == 0)
+{
+printf("error");
+return 0;
+}
+return a / b;
+}
+int mod(int a, int b)
+{
+if (b == 0)
+{
+printf("error");
+return 0;
+}
+return a % b;
+}
